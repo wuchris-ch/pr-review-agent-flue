6581d5bc0691b798b8a467e7ecbd0d6b9afde9b7
Chris <chriswu.ca@gmail.com>
2026-09-11 14:59:22 -0700

    Demo unsafe interpolated user lookup

---
+++ examples/user-lookup.mjs
@@ -0,0 +1,4 @@
+/** Look up users by the email supplied in an HTTP request. */
+export function findUsersByEmail(database, email) {
+  return database.prepare(`SELECT id, email FROM users WHERE email = '${email}'`).all();
+}
